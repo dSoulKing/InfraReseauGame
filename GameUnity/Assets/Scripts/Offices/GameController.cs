3867d5c24c5eeb12322c12a2662a1ce7b807900c
dSoulKing <thomas.deray@gmail.com>
2018-06-25 17:39:22 +0200

    travel random et worker qui suit OK

--- GameUnity/Assets/Scripts/Offices/GameController.cs
+++ GameUnity/Assets/Scripts/Offices/GameController.cs
@@ -3,21 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
-    public GameObject worker1;
-    public GameObject worker2;
-    public GameObject worker3;
-    public GameObject worker4;
-    public GameObject helpBubble;
+    public GameObject worker;
     public GameObject exclamationPoint1;
     public GameObject exclamationPoint2;
-    public GameObject clickToSetUp1;
-    public GameObject clickToSetUp2;
-    public GameObject clickToSetUp4;
-
-    public GameObject timerTextObject;
-    public Text timerText;
 
     public static bool boolComputer1;
     public static bool boolComputer2;
@@ -31,129 +22,32 @@ public class GameController : MonoBehaviour {
     public static int totalPoints;
     public Text pointText;
 
-    //private Vector3 positionWorker;
-    private Vector3 positionExclamationPoint;
-    private Vector3 positionHelpBubble;
-
-    private float timeNewWorker;
-    private float timeNewHelp;
-    private int numberWorker;
-    private int numberExclamationPoint;
-    private float eP1Malus;
-    private float eP2Malus;
-
+    private float newWorkerTime;
 
     void Start()
     {
-        boolSetUp1 = true;
-        boolSetUp2 = true;
-        boolSetUp3 = true;
-        timeNewWorker = 5f;
-        timeNewHelp = 15f;
-        eP1Malus = 5;
-        eP2Malus = 5;
+        newWorkerTime = 5;
+
+        boolComputer1 = false;
+        boolComputer2 = false;
 
         gamePause = false;
         totalPoints = 100;
         pointText.text = "Point :\n" + totalPoints + "/100";
 
-        numberWorker = 0;
-        numberExclamationPoint = 0;
-
-        //positionWorker = new Vector3(8.699707f, 4.293945f, 11.82227f);
-        positionExclamationPoint = new Vector3(1.5f, 2.22f, 7);
-        positionHelpBubble = new Vector3(1.735f, 1, 8.752f);
-
-        boolComputer1 = false;
-        boolComputer2 = false;
+        boolSetUp1 = true;
+        boolSetUp2 = true;
+        boolSetUp3 = true;
     }
 
     void Update()
     {
-        if (MenuStart.begin && !gamePause)
+        newWorkerTime -= Time.deltaTime;
+        if (newWorkerTime < 0)
         {
-            timeNewWorker -= Time.deltaTime;
-            timeNewHelp -= Time.deltaTime;
-        }
-
-        if (timeNewWorker <= 0)
-        {
-            numberWorker++;
-            if (numberWorker == 1)
-            {
-                Instantiate(worker1, worker1.transform.position, Quaternion.identity);
-                timeNewWorker = 25f;
-
-                Instantiate(clickToSetUp1, clickToSetUp1.transform.position, clickToSetUp1.transform.rotation);
-            }
-            else if (numberWorker == 2)
-            {
-                Instantiate(worker2, worker2.transform.position, Quaternion.identity);
-                timeNewWorker = 15f;
-
-                Instantiate(clickToSetUp2, clickToSetUp2.transform.position, clickToSetUp2.transform.rotation);
-            }
-            else if (numberWorker == 3)
-            {
-                Instantiate(worker3, worker3.transform.position, Quaternion.identity);
-                timeNewWorker = 5f;
-            }
-            else if (numberWorker == 4)
-            {
-                Instantiate(worker4, worker4.transform.position, Quaternion.identity);
-                timeNewWorker = 5000f;
-
-                Instantiate(clickToSetUp4, clickToSetUp4.transform.position, clickToSetUp4.transform.rotation);
-            }
-        }
-
-        if (timeNewHelp <= 0)
-        {
-            numberExclamationPoint++;
-            if (numberExclamationPoint == 1)
-            {
-                exclamationPoint1 = (GameObject) Instantiate(exclamationPoint1, positionExclamationPoint, exclamationPoint1.transform.rotation);
-                Instantiate(helpBubble, positionHelpBubble, helpBubble.transform.rotation);
-
-                timeNewHelp = 30f;
-                positionExclamationPoint = new Vector3(22.5f, 2.22f, 10.5f);
-                positionHelpBubble = new Vector3(22.397f, 1, 12.773f);
-
-                boolComputer1 = true;
-            }
-            else if (numberExclamationPoint == 2)
-            {
-                exclamationPoint2 = (GameObject) Instantiate(exclamationPoint2, positionExclamationPoint, exclamationPoint2.transform.rotation);
-                Instantiate(helpBubble, positionHelpBubble, helpBubble.transform.rotation);
-
-                timeNewHelp = 20f;
-                positionExclamationPoint = new Vector3(7f, 2.22f, 1.5f);
-                
-                boolComputer2 = true;
-            }
-        }
-
-        if (boolComputer1 && !VirusManager.virusStart)
-            eP1Malus -= Time.deltaTime;
-        if (boolComputer2 && !RepairManager.repairStart)
-            eP2Malus -= Time.deltaTime;
-        if (eP1Malus <= 0)
-        {
-            updateScore();
-            eP1Malus = 5;
-        }
-        if (eP2Malus <= 0)
-        {
-            updateScore();
-            eP2Malus = 5;
-        }
-
-        if (!boolComputer2)
-            timerTextObject.SetActive(false);
-        else
-        {
-            timerTextObject.SetActive(true);
-            timerText.text = RepairManager.timerText;
+            Debug.Log("if enter");
+            Instantiate(worker, worker.transform.position, worker.transform.rotation);
+            newWorkerTime = 5;
         }
     }
 
